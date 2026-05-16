ab533cfe208545b5d4d6ad9d8bfeed16d06b7507
QureshiAwais <awaisqureshi26765@gmail.com>
2026-05-16 14:28:48 +0530

    READEME UPDATE

--- src/components/header/LogoutBtn.jsx
+++ src/components/header/LogoutBtn.jsx
@@ -1,11 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import authService from "../../appwrite/auth";
 import { logout } from "../../store/authSlice";
 
 const LogoutBtn = () => {
   const dispatch = useDispatch();
-
   const logoutHandler = () => {
     authService
       .logout()
